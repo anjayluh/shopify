0f84619f8013ac16760c6acb7d99cf7698f10a43
angella <angella@laboremus.no>
2020-12-04 13:55:21 +0300

    Added static css file

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
