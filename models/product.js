84c458e8b14cbed7b100170b23d346fe70db68af
angella <angella@laboremus.no>
2020-12-05 15:11:14 +0300

    Added models, data and controllers

---
+++ models/product.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const path = require('path');
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+);
+
+const getProductsFromFile = callBack => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            return callBack([]);
+        }
+        callBack(JSON.parse(fileContent));
+    })
+}
+
+module.exports = class Product {
+    constructor(t) {
+        this.title = t;
+    }
+
+    save() {
+        getProductsFromFile((products) => {
+            products.push(this)
+            fs.writeFile(p, JSON.stringify(products), (err) => {});
+        });
+    }
+
+    static fetchAll(callBack) {
+        getProductsFromFile(callBack);
+    }
+}
